1522e83cf91e489f735112fd428f0c4ce2c3efbe
Harikant Bajaj <harikantb08@gmail.com>
2025-03-12 22:37:27 +0530

    JAVA_D1

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.print("hii");
+        System.out.print("hello");
+
+        //String name = harikant;
+        int age = 21;
+        System.out.println("the age is "+age);
+
+        double price = 33.22;
+
+        
+
+
+
+
+
+    }
+}
