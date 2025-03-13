aeb0ad1e79186c6ddb270926e793c074b17bcea6
Harikant Bajaj <harikantb08@gmail.com>
2025-03-13 23:22:53 +0530

    D2

--- Main.java
+++ Main.java
@@ -1,16 +1,30 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
-        System.out.print("hii");
-        System.out.print("hello");
+        //System.out.print("hii");
+        //System.out.print("hello");
 
+        //boolean isStudent = false;
         //String name = harikant;
-        int age = 21;
-        System.out.println("the age is "+age);
+        //int age = 21;
+        //System.out.println("the age is "+age);
 
-        double price = 33.22;
-
+        //if(isStudent){
+            //System.out.println("you are student");
         
+        //}else {
+            //System.out.println("you are not student");
+        //}
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("enter ur name");
+        String name= scanner.nextLine();
 
+        scanner.close();
+        System.out.println("hii  "+name);
+        
 
 
 
